2dcbcd63b30ab25fbfda3ea804bd3d5c6366b141
Nevis <nevishs@users.noreply.github.com>
2018-04-22 21:45:46 +0200

    Update runLamp.sh

--- scripts/runLamp.sh
+++ scripts/runLamp.sh
@@ -1,14 +1,11 @@
 echo "Getting the code from GitHub"
-
 git clone https://github.com/nevishs/DockerLamp.git /tmp/DockerLamp
 cd /tmp/DockerLamp
 
 echo "Docker pull";
-
 docker pull nevishs/lamp-server;
 
 echo "Docker run";
-
 docker run -d -p 80:80 -p3306:3306 -v /tmp/DockerLamp/www:/var/www nevishs/lamp-server; sleep 20; curl http://localhost/install.php;
 
 echo "Done";
